c1b893205ee1ac8fdc6d194b124d8fac9b797817
Will Ben <wilfrid.benoit@outlook.fr>
2023-04-20 10:48:23 +0200

    projet demo sur le binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-binding-2';
+
+  welcome = "Bienvenue sur mon site";
+
+  imgSrc = "https://via.placeholder.com/350x150";
+
+  isClicked() {
+    console.log("Coucou !");
+  }
+
+  user = {
+    quote: ""
+  }
+
+
 }
